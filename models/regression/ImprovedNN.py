b493bf4edd2ce51afcb6be8a2b38ca57291a8b72
qthen <philiptsang018@gmail.com>
2018-04-02 03:29:29 -0400

    Added in refactored models and visualizations

--- models/regression/ImprovedNN.py
+++ models/regression/ImprovedNN.py
@@ -1,22 +1,27 @@
-from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras.callbacks import EarlyStopping
 from keras.layers import Dense, Activation, Dropout, Input, BatchNormalization
 from keras.models import Model, load_model
 from keras.regularizers import l2
+import keras.backend as K
 import numpy as np
 import services.ModelPerformanceVisualization as model_visualizer
 
 '''
-A baseline neural network that consists of separate fully connected layers to a given star spectra and 
-does regression on asteroseismic parameters, and is easily adaptable to regression or binary classification
+A improved neural network that uses the advantages of ELU and is fine tuned on Kepler training data. 
+Best regression error on validation set is 18 MAE on PS
 
-Star spectra for this model should be dimensionally reduced prior to prediction due to the size of a large 
-fully connected layer and low count of training data
+Best model:
+	- PCA with 50 stellar components
+	- Normalized spectra similar to Cannon
+	- Normalized Π_1 and Δv
+
+Written: March 29, 2018
 '''
 
-class BaselineNN(object):
+class ImprovedNN(object):
 
 	# Constant for where to save this model
-	MODEL_FILEPATH = "models/regression/BaselineNN.h5"
+	MODEL_FILEPATH = "models/regression/ImprovedNN.h5"
 
 	'''
 	Creates the baseline NN with separate fully connected layers on stellar spectra
@@ -29,16 +34,28 @@ class BaselineNN(object):
 
 		# Create the model
 		input_spectra = Input(shape=(S_D,), name='input_spectra')
-
-		dense_layer_1 = Dropout(0.15)(Dense(128, activation='relu')(input_spectra))
-		dense_layer_2 = Dropout(0.15)(Dense(64, activation='relu')(dense_layer_1))
-		prediction_ps = Dense(1, activation='linear', name='DPi1')(dense_layer_2)
+		# Dense layers for PS
+		dense_layer_1 = Dense(128, activation='elu', kernel_regularizer=l2(1e-5))(input_spectra)
+		dense_layer_1 = Dropout(0.2)(dense_layer_1)
+		dense_layer_1 = Dense(64, activation='elu', kernel_regularizer=l2(1e-5))(dense_layer_1)
+		dense_layer_1 = Dropout(0.2)(dense_layer_1)
+		dense_layer_1 = Dense(64, activation='elu', kernel_regularizer=l2(1e-5))(dense_layer_1)
+		dense_layer_1 = Dropout(0.2)(dense_layer_1)
+		dense_layer_1 = Dense(64, activation='elu', kernel_regularizer=l2(1e-5))(dense_layer_1)
+		dense_layer_1 = Dropout(0.2)(dense_layer_1)
+		dense_layer_1 = Dense(32, activation='elu', kernel_regularizer=l2(1e-5))(dense_layer_1)
+		dense_layer_1 = Dropout(0.2)(dense_layer_1)
+		prediction_ps = Dense(1, activation='linear', name='DPi1')(dense_layer_1)
 
 		# Dense layers for Dnu
-		dense_layer_1 = Dropout(0.1)(Dense(64, activation='relu')(input_spectra))
-		dense_layer_2 = Dropout(0.1)(Dense(32, activation='relu')(dense_layer_1))
+		dense_layer_1 = Dense(32, activation='elu')(input_spectra)
+		dense_layer_1 = Dropout(0.1)(dense_layer_1)
+		dense_layer_2 = Dense(16, activation='elu')(dense_layer_1)
+		dense_layer_1 = Dropout(0.1)(dense_layer_1)
 		prediction_dnu = Dense(1, activation='linear', name='Dnu')(dense_layer_2)
-		self.model = Model(input=input_spectra, outputs=[prediction_ps, prediction_dnu])
+
+
+		self.model = Model(input=input_spectra, output=[prediction_ps, prediction_dnu])
 
 	'''
 	Compile the model given the optimizer, loss function, and the model architecture and metrics
@@ -68,7 +85,7 @@ class BaselineNN(object):
 	def fit(self, X, y, epochs = 10, batch_size = 32, validation_split = 0.1, save_best_val = False):
 		callbacks = []
 		if save_best_val:
-			callbacks.append(ModelCheckpoint(BaselineNN.MODEL_FILEPATH, save_best_only=True, monitor='val_loss'))
+			callbacks.append(ModelCheckpoint(ImprovedNN.MODEL_FILEPATH, save_best_only=True, monitor='val_loss'))
 
 		# Fit the model
 		fit_results = self.model.fit(X, y, epochs = epochs, batch_size = batch_size, validation_split = validation_split, shuffle = True, callbacks = callbacks)
@@ -82,6 +99,16 @@ class BaselineNN(object):
 
 
 	'''
+	Predicts on a given test data sample
+	Inputs:
+		X - The spectra data
+	Returns:
+		array of nparray, [0] => PS, [1] => Δv
+	'''
+	def predict(self, X):
+		return self.model.predict(X)
+
+	'''
 	Returns a tuple of the model's performance based on the loss function value and the metrics given to it when compiled
 	Inputs:
 		X - Stellar spectra (m x d) ndarray
@@ -93,23 +120,13 @@ class BaselineNN(object):
 		return self.model.evaluate(X, y)
 
 	'''
-	Predicts on a given test data sample
-	Inputs:
-		X - The spectra data
-	Returns:
-		array of nparray, [0] => PS, [1] => Δv
-	'''
-	def predict(self, X):
-		return self.model.predict(X)
-
-	'''
 	Saves the model into its constant defined file
 	'''
 	def save(self):
-		self.model.save(BaselineNN.MODEL_FILEPATH)
+		self.model.save(ImprovedNN.MODEL_FILEPATH)
 
 	'''
 	If called, loads the current saved model as the starting point
 	'''
 	def load(self):
-		self.model = load_model(BaselineNN.MODEL_FILEPATH)
+		self.model = load_model(ImprovedNN.MODEL_FILEPATH)
--- services/ModelPerformanceVisualization.py
+++ services/ModelPerformanceVisualization.py
@@ -1,6 +1,131 @@
 # A collection of functions to visualize a model's performance
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.preprocessing import scale
+
+'''
+Plots the Δv - Π_1 space given the status of each red giant
+Input:
+	dnu - DNU parameters
+	ps - PS parameters
+	status - The status of each red giant, 0 for rgb, >= 1 for RC
+'''
+def plot_dnu_ps_space(dnu, ps, status, title="Δv - Π_1 space"):
+	N = len(dnu)
+	rgb_indices = [i for i in range(0, N) if status[i] == 0]
+	rc_indices = [i for i in range(0, N) if status[i] >= 1]
+	ax_rgb = plt.scatter(dnu[rgb_indices], ps[rgb_indices],  color="#F03434", alpha=0.5, marker='v')
+	ax_rc = plt.scatter(dnu[rc_indices], ps[rc_indices],  color="#F89406", alpha=0.5)
+	plt.legend((ax_rgb, ax_rc), ("RGB", "RC"))
+	plt.title(title)
+	plt.xlabel("Δv - Large frequency separation")
+	plt.ylabel("Π_1 - Period spacing")
+
+
+'''
+Plots useful plots of a difficult star in respect to many spaces
+
+Current process is to take a given stellar spectra
+Truth and regression space expects the following spaces in order (as a list)
+	Δv, Π_1, log(g), T_eff, status
+
+These spaces are assumed to be non-normalized
+'''
+def plot_difficult_star(kic, target_idx, spectral_space, truth_space, regression_space):
+	N = len(spectral_space)
+	truth_dnu, truth_ps, truth_logg, truth_teff, truth_rc = truth_space
+	reg_dnu, reg_ps, reg_logg, reg_teff, reg_rc = regression_space
+
+	truth_rgb_indices = [i for i in range(0, N) if truth_rc[i] == 0]
+	truth_rc_indices = [i for i in range(0, N) if truth_rc[i] >= 1]
+
+	target_spectra = spectral_space[target_idx]
+
+	# Find the closest star to this star in spectral space and plot it on the log(g)-Teff diagram
+	spectral_closest_idx = k_closest_vectors(spectral_space, target_spectra, k = 10)
+	spectral_closest_idx = [idx for idx in spectral_closest_idx if idx != target_idx]
+	spectral_closest_rc_idx = [idx for idx in spectral_closest_idx if truth_rc[idx] >= 1]
+	spectral_closest_rgb_idx = [idx for idx in spectral_closest_idx if truth_rc[idx] == 0]
+
+	# Plot the spectra space in the background, faded
+	plt.scatter(truth_teff[truth_rgb_indices], truth_logg[truth_rgb_indices], color="#F03434", alpha=0.05, marker='v')
+	plt.scatter(truth_teff[truth_rc_indices], truth_logg[truth_rc_indices], color="#F89406", alpha=0.05)
+	rgb_ax = plt.scatter(truth_teff[spectral_closest_rgb_idx], truth_logg[spectral_closest_rgb_idx], color="#F03434", alpha=1, marker='v')
+	rc_ax = plt.scatter(truth_teff[spectral_closest_rc_idx], truth_logg[spectral_closest_rc_idx], color="#F89406", alpha=1)
+	target_ax = plt.scatter(truth_teff[target_idx], truth_logg[target_idx], color="#3498DB", marker='s')
+
+	# Annotate distances from each star
+	for idx in spectral_closest_idx:
+		plt.annotate("{:.2f}".format(np.linalg.norm(spectral_space[idx] - spectral_space[target_idx])), (truth_teff[idx], truth_logg[idx]))
+	plt.xlim(5200, 4500)
+	plt.ylim(3.5, 2.25)
+	plt.xlabel("Effective temperature - T_eff")
+	plt.ylabel("Surface gravity - log(g)")
+	plt.title("KIC {} closest giants in spectral space".format(kic))
+	plt.legend((rgb_ax, rc_ax, target_ax), ("RGB", "RC", "KIC {}".format(kic)))
+	plt.show()
+
+	# Find the closest stars in regression space based on regression parameters and then based on truth parameters
+	dnu_ps_reg_space = np.column_stack([scale(reg_dnu), scale(reg_ps)])
+	dnu_ps_truth_space = np.column_stack([scale(truth_dnu), scale(truth_ps)])
+	closest_idx_reg_params = k_closest_vectors(dnu_ps_reg_space, dnu_ps_reg_space[target_idx], k = 5)
+	closest_idx_truth_params = k_closest_vectors(dnu_ps_reg_space, dnu_ps_truth_space[target_idx], k = 5)
+	closest_idx_reg_params = [idx for idx in closest_idx_reg_params if idx != target_idx]
+	closest_idx_truth_params = [idx for idx in closest_idx_truth_params if idx != target_idx]
+
+	# Show where the closest stars are
+	plt.scatter(reg_dnu[truth_rgb_indices], reg_ps[truth_rgb_indices], color="#F03434", alpha=0.05, marker='v')
+	plt.scatter(reg_dnu[truth_rc_indices], reg_ps[truth_rc_indices], color="#F89406", alpha=0.05)
+	reg_ax = plt.scatter(reg_dnu[closest_idx_reg_params], reg_ps[closest_idx_reg_params], color="#F89406")
+	truth_ax = plt.scatter(reg_dnu[closest_idx_truth_params], reg_ps[closest_idx_truth_params], color="#F03434")
+	target_ax = plt.scatter(reg_dnu[target_idx], reg_ps[target_idx], color="#3498DB", marker='s')
+	target_true_ax = plt.scatter(truth_dnu[target_idx], truth_ps[target_idx], color="#3498DB", marker='p')
+	plt.title("Where the closest stars came from in Δv - Π_1 regression space")
+	plt.xlabel("Δv - large frequency separation")
+	plt.ylabel("Π_1 - Period spacing")
+	plt.legend((truth_ax, reg_ax, target_ax), ("Closest giants to true params", "Closest giants to regressed params", "Regressed KIC {}".format(kic), "True KIC {}".format(kic)))
+	plt.show()
+
+
+
+'''
+Returns k closest vectors to some target vector given a list of points in R^n
+Input:
+	space - The list of points in R^n
+	target - The target vector in R^n
+	k - k closest vectors to return
+Output: indices of closest vectors
+'''
+def k_closest_vectors(space, target, k = 5):
+	d = np.linalg.norm(space - target, axis = 1)
+	return d.argsort()[0:k]
+
+'''
+Plots performance of binary classification into logg-Teff space
+Correctly labelled stars are faint (alpha = 0.1) while incorrect labelled stars are opaque
+Input:
+	logg - x-axis
+	teff - y-axis
+	ground_truth - Ground truth for labels
+	probabilities - Predicted probabilities of labels
+	title - The title of the scatter plot
+'''
+def plot_binary_classification(logg, teff, ground_truth, probabilities, title="Binary Classification"):
+	rc_stars = np.array([[teff[i], logg[i]] for i in range(0, min(len(logg), len(teff))) if ground_truth[i] == 1 and probabilities[i] >= 0.5])
+	rgb_stars = np.array([[teff[i], logg[i]] for i in range(0, min(len(logg), len(teff))) if ground_truth[i] == 0 and probabilities[i] < 0.5])
+	rc_stars_misclassified = np.array([[teff[i], logg[i]] for i in range(0, min(len(logg), len(teff))) if ground_truth[i] == 1 and probabilities[i] < 0.5])
+	rgb_stars_misclassified = np.array([[teff[i], logg[i]] for i in range(0, min(len(logg), len(teff))) if ground_truth[i] == 0 and probabilities[i] >= 0.5])
+	plt.title(title)
+	plt.xlim(5200, 4500)
+	plt.ylim(3.5, 2.25)
+	rc_ax = plt.scatter(rc_stars[:,0], rc_stars[:,1], color="#F89406", alpha=0.05)
+	rgb_ax = plt.scatter(rgb_stars[:,0], rgb_stars[:,1], color="#F03434", alpha=0.05)
+	rgb_ax_mis = plt.scatter(rgb_stars_misclassified[:,0], rgb_stars_misclassified[:,1], color="#F03434", alpha=1, marker='v')
+	if len(rc_stars_misclassified) == 0:
+		rc_ax_mis = plt.scatter([], [], color="#F89406", alpha=1, marker='^')
+	else:
+		rc_ax_mis = plt.scatter(rc_stars_misclassified[:,0], rc_stars_misclassified[:,1], color="#F89406", alpha=1, marker='^')
+	plt.legend((rc_ax, rgb_ax, rgb_ax_mis, rc_ax_mis), ('RC stars classified correctly', 'RGB stars classified correctly', 'RGB stars mislassified', 'RC stars misclassified'))
 
 '''
 Plots the predicted PS with the truth PS
